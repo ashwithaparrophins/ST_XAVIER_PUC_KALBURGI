4d3308f04c1d8292267d99bc66be98abfc798faa
vikyathparrophins <92969037+vikyathparrophins@users.noreply.github.com>
2024-04-19 15:48:02 +0530

    ANNUAL MARK CARD

--- STAFF_PORTAL/application/views/examReport22/generateAnnualMarkCardNew.php
+++ STAFF_PORTAL/application/views/examReport22/generateAnnualMarkCardNew.php
@@ -552,11 +552,11 @@ foreach ($studentsRecords as $record) {
                         <td style="">ಪಡೆದ ದರ್ಜೆ<br>Class Obtained &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b class="text-center"><?php echo $final_result; ?></b></td>
                         <!-- <td></td> -->
                     </tr>
-                    <tr class="text-left" style="font-size: 16px;">
+                    <!-- <tr class="text-left" style="font-size: 16px;">
                         <td  style="padding:7px !important;text-align: left!important;" class="text-left">&nbsp;ಕಾಲೇಜು ಸಂಕೇತ ಸಂಖ್ಯೆ<br>&nbsp;College Code No.<b>&nbsp;&nbsp;&nbsp;KK0267 </b></td>
                         <td colspan="7" style=""> &nbsp;ಕಾಲೇಜು &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>ST XAVIER'S PRE–UNIVERSITY COLLEGE</b><br>&nbsp;College &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:11px"><b>N.H 50, SIRNOOR KALABURAGI - 585308</b></span></td>
-                        <!-- <td></td> -->
-                    </tr>
+                        <!-- <td></td>
+                    </tr> -->
                 </table>
 
                 <table style="font-size: 12px; margin: 8px 20px 1px 20px;">
