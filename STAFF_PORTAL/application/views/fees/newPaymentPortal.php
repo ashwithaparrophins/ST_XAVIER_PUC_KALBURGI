98285c70be87fe3608bf1b2e59df07cdfdc1828c
Dhanush <146809176+DHANUSHKULAL12@users.noreply.github.com>
2024-07-04 12:13:13 +0530

    Fees II PUC adding new student pending amount of i puc removed

--- STAFF_PORTAL/application/views/fees/newPaymentPortal.php
+++ STAFF_PORTAL/application/views/fees/newPaymentPortal.php
@@ -267,7 +267,7 @@ if ($error) {
                                                         </tr>
                                                         <?php } ?>
 
-                                                    <?php if ($term_name == "II PUC") { ?>
+                                                    <?php if ($term_name == "II PUC" && $studentInfo->intake_year != '2024-25') { ?>
                                                     <tr class="table-danger text-white">
                                                         <th class="text-left" scope="col">I PUC Balance</th>
                                                         <th class="text-left" scope="col">
