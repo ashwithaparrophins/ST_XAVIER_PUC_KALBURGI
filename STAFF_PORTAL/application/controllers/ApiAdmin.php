643a33b0131760d4870623b607e6cb93f88a32cb
Ajay-Parrophins <ajay@parrophins.com>
2024-07-07 23:00:03 +0530

    :Added: alumni count and bus count api created

--- STAFF_PORTAL/application/controllers/ApiAdmin.php
+++ STAFF_PORTAL/application/controllers/ApiAdmin.php
@@ -12,6 +12,7 @@ class ApiAdmin extends CI_Controller
         parent::__construct();
         $this->load->model('staff_model','staff');
         $this->load->model('Students_model','student');
+        $this->load->model('Transport_model','transport');
     }
 
 
@@ -22,6 +23,18 @@ class ApiAdmin extends CI_Controller
         $filter = array();
         // $result = $this->staff->getAllSchoolStaffInfo();
         $result = $this->student->getCountOfStudents($filter);
+        $data = json_encode($result);
+        echo $data;
+    }
+
+    public function GetAlumniStudentCountApi()
+    {
+        log_message('debug', 'I HAVE BEEN HIT');
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json, true);     
+        // $result = $this->staff->getAllSchoolStaffInfo();
+        $filter = array();
+        $result = $this->student->getAlumniStudentCount($filter);
         $data = json_encode($result);
         echo $data;
     }
@@ -92,6 +105,18 @@ class ApiAdmin extends CI_Controller
         $obj = json_decode($json, true);
         $baseUrl = base_url();
         $data = json_encode(['baseUrl' => $baseUrl]); // Encode the base URL in a JSON object.
+        echo $data;
+    }
+
+    public function getBusCount()
+    {
+        log_message('debug', 'I HAVE BEEN HIT STAFF');
+
+        $json = file_get_contents('php://input');
+        $obj = json_decode($json, true);
+        $filter = array();
+        $result = $this->transport->getAllBusCount($filter);
+        $data = json_encode($result);
         echo $data;
     }
 
